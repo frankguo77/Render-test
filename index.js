1df9a6c76511ebdda8a11823acb779095724b85a
frankguo77 <guozhi824@163.com>
2024-01-11 08:43:01 +0000

    minior change

--- index.js
+++ index.js
@@ -82,7 +82,7 @@ app.get('/info', (request, response) => {
     response.send(`<p>Ponebook has info for ${persons.length} people</p>${new Date().toString()}`)
 })
 
-const PORT = 3001
+const PORT = process.env.PORT || 3001
 app.listen(PORT,() => {
     console.log(`Server running on port ${PORT}`)
 })
