5bfbdf70ffb4950c9fab93961549698626c13d82
Frank <guozhi824@163.com>
2024-01-19 03:19:08 +0000

    uibuild

--- index.js
+++ index.js
@@ -13,34 +13,6 @@ app.use(morgan(':method :url :status :res[content-length] - :response-time ms :b
 app.use(cors())
 app.use(express.static('dist'))
 
-
-let persons = [
-    // {
-    //     id: 1,
-    //     name:"Arto Hellas",
-    //     number: "040-12345"
-    // },
-    // {
-    //     id: 2,
-    //     name:"Ada Lovelace",
-    //     number: "39-44-5233523"
-    // },
-    // {
-    //     id: 3,
-    //     name:"Dan Abramov",
-    //     number: "12-43-234345"
-    // },
-    // {
-    //     id: 4,
-    //     name:"Mary Proppendick",
-    //     number: "39-23-6423122"
-    // },
-]
-
-// app.get('/', (request, response) => {
-//     response.send('<h1>Hello World!</h1>')
-// })
-
 app.get('/api/persons', (request, response) => {
     Person.find({}).then(persons => {
         response.json(persons)
